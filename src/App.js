ab4e2c5251cc92edc8ee576c5dd3969c5f4cea7c
Joshua Shrestha <33375383+joshrestha7@users.noreply.github.com>
2021-02-24 01:05:26 -0500

    created cognito trigger and link token endpoint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, {API, Auth} from 'aws-amplify';
 import config from './aws-exports'
 import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
+import { useEffect } from 'react';
 
 Amplify.configure(config)
 
 function App() {
+  useEffect(()=>{
+
+    Auth.currentAuthenticatedUser().then((user) => {
+      API.post('piggybankapi', '/create-link-token', {
+        body: {
+          id: user.attributes.sub
+        }
+      }).then(data => console.log(data), err=>console.log(err));
+    });
+  });
   return (
     <div className="App">
       <header className="App-header">
